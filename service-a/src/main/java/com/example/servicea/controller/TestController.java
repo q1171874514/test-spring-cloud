07182681b8a26ca725f16a06e7efe373ba58f72a
user <1171874514@qq.com>
2021-12-16 15:10:12 +0800

    增加服务保护（Hystrix）

--- service-a/src/main/java/com/example/servicea/controller/TestController.java
+++ service-a/src/main/java/com/example/servicea/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.servicea.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,19 @@ public class TestController {
     @Value("${server.port}")
     private String port;
 
+    //.在接口上配置服务保护（fallback是方法）
+    @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping("testA")
     public String textA() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "String Cloud textA 端口号：" + port;
+    }
+
+    String fallback(){
+        return "服务器繁忙";
     }
 }
