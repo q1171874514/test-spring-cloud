c8ac79327d5ee78f8822131cb7675d9b65718093
user <1171874514@qq.com>
2021-12-21 11:13:19 +0800

    增加熔断器（resilience4j）

--- service-a/src/test/java/com/example/servicea/ServiceAApplicationTests.java
+++ service-a/src/test/java/com/example/servicea/ServiceAApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.servicea;
 
+import com.example.servicea.controller.TestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,8 @@ class ServiceAApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+
 	}
 
 }
--- service-gateway/src/main/java/com/example/servicegateway/filter/TokenGatewayFilterFactory.java
+++ service-gateway/src/main/java/com/example/servicegateway/filter/TokenGatewayFilterFactory.java
@@ -1,7 +1,6 @@
 package com.example.servicegateway.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Maps;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -19,6 +19,12 @@ import java.util.Map;
  * @author 吕时海
  * @date 2021/12/20 14:28
  * @explain token过滤器
+ * 过滤器类型：Gateway实现方式上，有两种过滤器；
+ * 1. 局部过滤器：通过 spring.cloud.gateway.routes.filters 配置在具体路由下，只作用在当前路由
+     * 上；自带的过滤器都可以配置或者自定义按照自带过滤器的方式。如果配置
+     * spring.cloud.gateway.default-filters 上会对所有路由生效也算是全局的过滤器；但是这些过滤器
+     * 的实现上都是要实现GatewayFilterFactory接口。
+ * 2. 全局过滤器：不需要在配置文件中配置，作用在所有的路由上；实现 GlobalFilter 接口即可。
  */
 @Component
 public class TokenGatewayFilterFactory implements GlobalFilter, Ordered {
@@ -29,10 +35,9 @@ public class TokenGatewayFilterFactory implements GlobalFilter, Ordered {
         ServerHttpResponse response = exchange.getResponse();
         if(token == null) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-
             //令牌不存在
             //返回错误信息
-            Map<String,Object> responseData = Maps.newHashMap();
+            Map<String,Object> responseData = new HashMap();
             responseData.put("code",401);
             responseData.put("message","token不存在");
             responseData.put("cause","token is null");
